ed9b013bc71a26155741736973e8eaeac5ee4a10
ilhammuradov <muradovilham039@gmail.com>
2024-01-22 19:12:14 +0400

    init SeasonApp

---
+++ src/main/java/az/edu/turing/lesson5/LengthApp.java
@@ -0,0 +1,14 @@
+package az.edu.turing.lesson5;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class LengthApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the word: ");
+        String word= scanner.nextLine();
+        System.out.println(word.length()*2);
+    }
+}
