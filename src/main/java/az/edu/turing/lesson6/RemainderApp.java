658b5941fda9913b1cc64c32a4c79e7aa9ede133
ilhammuradov <muradovilham039@gmail.com>
2024-01-22 19:39:53 +0400

    init SeasonApp

--- src/main/java/az/edu/turing/lesson6/RemainderApp.java
+++ src/main/java/az/edu/turing/lesson6/RemainderApp.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class RemainderApp {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the numbers: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println(a%b);
+        System.out.println(a % b);
     }
 }
