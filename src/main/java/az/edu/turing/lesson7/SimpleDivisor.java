d9832d0ba78859778796cf0a9deaf7877ebe4015
ilhammuradov <muradovilham039@gmail.com>
2024-01-26 17:47:35 +0400

    simple divisor

--- src/main/java/az/edu/turing/lesson7/SimpleDivisor.java
+++ src/main/java/az/edu/turing/lesson7/SimpleDivisor.java
@@ -7,7 +7,7 @@ public class SimpleDivisor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int k=n;
+        int k = n;
         if (n <= 1) {
             System.out.println("No simple divisors");
         } else {
@@ -20,9 +20,9 @@ public class SimpleDivisor {
         }
         System.out.println();
         System.out.println("Even and odd divisors: ");
-        for (int i=2;i<=k;i++){
-            if(k % i == 0 && i%2==0) System.out.println("even: "+i+" ");
-            if(k % i == 0 && i%2!=0) System.out.println("odd: "+i+" ");
+        for (int i = 2; i <= k; i++) {
+            if (k % i == 0 && i % 2 == 0) System.out.println("even: " + i + " ");
+            if (k % i == 0 && i % 2 != 0) System.out.println("odd: " + i + " ");
         }
 
     }
