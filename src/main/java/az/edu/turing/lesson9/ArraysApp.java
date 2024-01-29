abe325555caa738fa0081fc0d7777c54f9ce18d8
ilhammuradov <muradovilham039@gmail.com>
2024-01-29 20:37:44 +0400

    ArrayApp

--- src/main/java/az/edu/turing/lesson9/ArraysApp.java
+++ src/main/java/az/edu/turing/lesson9/ArraysApp.java
@@ -6,19 +6,19 @@ public class ArraysApp {
 
     public static void main(String[] args) {
         Random random = new Random();
+        int[] arr = new int[4];
         for (int i = 0; i < 3; i++) {
-            int[] arr = new int[4];
             for (int j = 0; j < 4; j++) {
                 arr[j] = random.nextInt(101);
-                printArrays(arr);
             }
+            printArrays(arr);
             System.out.println();
         }
     }
 
     public static void printArrays(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Index: " + i + "Value: " + arr[i]);
+            System.out.println("Index: " + i + " Value: " + arr[i]);
         }
     }
 
