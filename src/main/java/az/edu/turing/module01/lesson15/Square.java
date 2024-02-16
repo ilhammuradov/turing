2baf383e374cf81ca53fbed98b40f2163c441efd
ilhammuradov <muradovilham039@gmail.com>
2024-02-16 20:07:01 +0400

    module01 attached

--- src/main/java/az/edu/turing/module01/lesson15/Square.java
+++ src/main/java/az/edu/turing/module01/lesson15/Square.java
@@ -2,32 +2,32 @@ package az.edu.turing.module01.lesson15;
 
 public class Square {
 
-  private   String color;
-   private final double side;
-   public static long countOfSquares;
+    public static long countOfSquares;
+    private final double side;
+    private String color;
 
-   public Square(double side){
-       this.side=side;
-       countOfSquares++;
-   }
+    public Square(double side) {
+        this.side = side;
+        countOfSquares++;
+    }
 
     public double getSide() {
         return side;
-    }
-
-    public void setColor(String color) {
-        if (!"white".equals(color)) this.color=color;
     }
 
     public String getColor() {
         return color;
     }
 
-    public double calculateLength(){
-        return this.side*4;
+    public void setColor(String color) {
+        if (!"white".equals(color)) this.color = color;
     }
 
-    public double calculateArea(){
-        return this.side*this.side;
+    public double calculateLength() {
+        return this.side * 4;
+    }
+
+    public double calculateArea() {
+        return this.side * this.side;
     }
 }
